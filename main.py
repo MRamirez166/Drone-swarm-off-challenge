31f548deaeb5a45fbe9b2e7e79c4fc6cadce839e
Naimur Abir <144700345+Naimur-Abir@users.noreply.github.com>
2024-01-10 10:42:55 -0500

    still working on the microphone

--- main.py
+++ main.py
@@ -1,16 +1,88 @@
-# from codrone_edu.drone import *
+from codrone_edu.drone import *
+
 import speech_recognition as sr
+
 import pyttsx3
 
+drone = Drone()
 
-# drone = Drone()
+
+def straight_line():
+    drone.pair()
+    drone.takeoff()
+    # Drone goes up for 1 second with 50 power
+    drone.set_pitch(70)
+    drone.avoid_wall(50, 50)
+    drone.set_pitch(-70)
+    drone.turn_degree(180)
+    drone.set_pitch(70)
+    drone.move(4.5)
+
+    drone.land()
+
+    drone.close()
+
+
+def spiral():
+    drone.pair()
+    drone.takeoff()
+    # default spiral parameters (50, 5, 1)
+    drone.spiral(20, 5, 1)
+    drone.land()
+    drone.close()
+
+
+def figure_8():
+    drone.pair()
+    drone.takeoff()
+    drone.set_yaw(50)
+    drone.set_pitch(70)
+    drone.move(1.5)
+    drone.set_yaw(-100)
+    drone.move(3)
+    drone.set_yaw(100)
+    drone.move(2)
+    drone.set_yaw(50)
+    drone.move(1.5)
+    drone.set_yaw(-100)
+    drone.move(3)
+    drone.set_yaw(100)
+    drone.move(2)
+    drone.land()
+    drone.close()
+
+
+def loop():
+    drone.pair()
+    drone.takeoff()
+    # Drone goes up for 1 second with 50 power
+    drone.set_throttle(50)
+    drone.set_pitch(50)
+    drone.move(1.5)  # move command executes the movement for 1 second
+    drone.set_throttle(-50)
+    drone.set_pitch(-100)
+    drone.move(1.5)
+    drone.land()
+
+    drone.close()
+
+# Loop infinitely for user to
+
+
 r = sr.Recognizer()
-
 
 # Function to convert text to
 # speech
-while (1):
 
+
+def SpeakText(command):
+    # Initialize the engine
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
+
+
+while (1):
     # Exception handling to handle
     # exceptions at the runtime
     try:
@@ -38,84 +110,16 @@ while (1):
 
     except sr.UnknownValueError:
         print("unknown error occurred")
+    # speak
 
 
 # while True:
-def SpeakText(command):
-    # Initialize the engine
-    engine = pyttsx3.init()
-    engine.say(command)
-    engine.runAndWait()
-
-
-# Loop infinitely for user to
-# speak
-
-
-# def straight_line():
-#     drone.pair()
-#     drone.takeoff()
-#     # Drone goes up for 1 second with 50 power
-#     drone.set_pitch(70)
-#     drone.avoid_wall(50, 50)
-#     drone.set_pitch(-70)
-#     drone.turn_degree(180)
-#     drone.set_pitch(70)
-#     drone.move(4.5)
-#
-#     drone.land()
-#
-#     drone.close()
-#
-#
-# def spiral():
-#     drone.pair()
-#     drone.takeoff()
-#     # default spiral parameters (50, 5, 1)
-#     drone.spiral(20, 5, 1)
-#     drone.land()
-#     drone.close()
-#
-# def figure_8():
-#     drone.pair()
-#     drone.takeoff()
-#     drone.set_yaw(50)
-#     drone.set_pitch(70)
-#     drone.move(1.5)
-#     drone.set_yaw(-100)
-#     drone.move(3)
-#     drone.set_yaw(100)
-#     drone.move(2)
-#     drone.set_yaw(50)
-#     drone.move(1.5)
-#     drone.set_yaw(-100)
-#     drone.move(3)
-#     drone.set_yaw(100)
-#     drone.move(2)
-#     drone.land()
-#     drone.close()
-#
-#
-# def loop():
-#     drone.pair()
-#     drone.takeoff()
-#     # Drone goes up for 1 second with 50 power
-#     drone.set_throttle(50)
-#     drone.set_pitch(50)
-#     drone.move(1.5)  # move command executes the movement for 1 second
-#     drone.set_throttle(-50)
-#     drone.set_pitch(-100)
-#     drone.move(1.5)
-#     drone.land()
-#
-#     drone.close()
 #     user_input = input("What would you like to do?")
-    # if user_input == "Figure 8":
-    #     figure_8()
-    # elif user_input == "Straight line":
-    #     straight_line()
-    # elif user_input == "Loop":
-    #     loop()
-    # elif user_input == "Spiral":
-    #     spiral()
-
+#     if user_input == "Figure 8":
+#         figure_8()
+#     elif user_input == "Straight line":
+#         straight_line()
+#     elif user_input == "Loop":
+#         loop()
+#     elif user_input == "Spiral":
+#         spiral()
