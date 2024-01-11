f57909bfaab347a091af2f028f3693055997f54b
Naimur Abir <144700345+Naimur-Abir@users.noreply.github.com>
2024-01-11 10:48:33 -0500

    Almost done with the mic working on the z aixs

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from codrone_edu.drone import *
 
-import speech_recognition as sr
-
-import pyttsx3
-
 drone = Drone()
 
 
@@ -66,60 +62,32 @@ def loop():
 
     drone.close()
 
-# Loop infinitely for user to
-
-
-r = sr.Recognizer()
-
-# Function to convert text to
-# speech
-
-
-def SpeakText(command):
-    # Initialize the engine
-    engine = pyttsx3.init()
-    engine.say(command)
-    engine.runAndWait()
-
-
-while (1):
-    # Exception handling to handle
-    # exceptions at the runtime
-    try:
-
-        # use the microphone as source for input.
-        with sr.Microphone() as source2:
-
-            # wait for a second to let the recognizer
-            # adjust the energy threshold based on
-            # the surrounding noise level
-            r.adjust_for_ambient_noise(source2, duration=0.2)
-
-            # listens for the user's input
-            audio2 = r.listen(source2)
-
-            # Using google to recognize audio
-            MyText = r.recognize_google(audio2)
-            MyText = MyText.lower()
-
-            print("Did you say ", MyText)
-            SpeakText(MyText)
-
-    except sr.RequestError as e:
-        print("Could not request results; {0}".format(e))
-
-    except sr.UnknownValueError:
-        print("unknown error occurred")
-    # speak
-
 
 # while True:
-#     user_input = input("What would you like to do?")
-#     if user_input == "Figure 8":
-#         figure_8()
-#     elif user_input == "Straight line":
-#         straight_line()
-#     elif user_input == "Loop":
-#         loop()
-#     elif user_input == "Spiral":
-#         spiral()
+# user_input = input("What would you like to do?")
+# if user_input == "Figure 8":
+#     figure_8()
+# elif user_input == "Straight line":
+#     straight_line()
+# elif user_input == "Loop":
+#     loop()
+# elif user_input == "Spiral":
+#     spiral()
+distance_difference = 150
+
+drone.pair()
+print(drone.get_bottom_range())
+bottom_range = drone.get_bottom_range()
+print(drone.get_bottom_range())
+drone.takeoff()
+print(drone.get_bottom_range())
+drone.set_throttle(30)
+print(drone.get_bottom_range())
+if distance_difference == int(bottom_range):
+    print(drone.get_bottom_range())
+    drone.set_throttle(-30)
+    print(drone.get_bottom_range())
+drone.land()
+print(drone.get_bottom_range())
+drone.close()
+print(drone.get_bottom_range())
