c257f5e17a08741a6cbe16a869db39377b395f76
MRamirez166 <144700597+MRamirez166@users.noreply.github.com>
2024-01-16 10:38:23 -0500

    Update speech_to_text.py

--- speech_to_text.py
+++ speech_to_text.py
@@ -7,17 +7,16 @@ recognizer.energy_threshold = 300  # sets the threshold in what I believe is dec
 mic = sr.Microphone()
 
 
-def return_voice():
-    with mic as source:
-        print("Say something\n")
-        recognizer.adjust_for_ambient_noise(source)  # adjust for noise
-        print("ready to record\n")
-        audio = recognizer.listen(source)  # listens to the person talking
-        print("audio captured\n")
-        try:
-            text = recognizer.recognize_google(audio)  # sets the outcome to a variable to print
-        except sr.UnknownValueError:  # incase we get an error
-            print("could not understand")
-        except sr.RequestError:
-            print("could not understand")  # incase we get an error
-        return text
+with mic as source:
+    print("Say something\n")
+    recognizer.adjust_for_ambient_noise(source)  # adjust for noise
+    print("ready to record\n")
+    audio = recognizer.listen(source)  # listens to the person talking
+    print("audio captured\n")
+    try:
+        text = recognizer.recognize_google(audio)  # sets the outcome to a variable to print
+        print(text)
+    except sr.UnknownValueError:  # incase we get an error
+        print("could not understand")
+    except sr.RequestError:
+        print("could not understand")  # incase we get an error
