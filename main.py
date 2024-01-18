1ec43035634b7a8349bc97e6b472176622445a90
Naimur Abir <144700345+Naimur-Abir@users.noreply.github.com>
2024-01-18 10:10:27 -0500

    Finished voice commands

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import speech_recognition as sr
 from codrone_edu.drone import *
-from speech_to_text import *
+from pydub import AudioSegment
+
+recognizer = sr.Recognizer()  # sets the recognizer to a variable
+recognizer.energy_threshold = 300  # sets the threshold in what I believe is decibles
+mic = sr.Microphone()
+
 drone = Drone()
 
-return_voice()
+recognizer = sr.Recognizer()  # sets the recognizer to a variable
+recognizer.energy_threshold = 300  # sets the threshold in what I believe is decibles
+mic = sr.Microphone()
+drone.pair()
 
 
 def straight_line():
-    drone.pair()
-    drone.takeoff()
     # Drone goes up for 1 second with 50 power
     drone.set_pitch(70)
     drone.avoid_wall(50, 50)
@@ -16,23 +23,15 @@ def straight_line():
     drone.set_pitch(70)
     drone.move(4.5)
 
-    drone.land()
 
-    drone.close()
 
 
 def spiral():
-    drone.pair()
-    drone.takeoff()
     # default spiral parameters (50, 5, 1)
     drone.spiral(20, 5, 1)
-    drone.land()
-    drone.close()
 
 
 def figure_8():
-    drone.pair()
-    drone.takeoff()
     drone.set_yaw(50)
     drone.set_pitch(70)
     drone.move(1.5)
@@ -46,13 +45,9 @@ def figure_8():
     drone.move(3)
     drone.set_yaw(100)
     drone.move(2)
-    drone.land()
-    drone.close()
 
 
 def loop():
-    drone.pair()
-    drone.takeoff()
     # Drone goes up for 1 second with 50 power
     drone.set_throttle(50)
     drone.set_pitch(50)
@@ -60,36 +55,47 @@ def loop():
     drone.set_throttle(-50)
     drone.set_pitch(-100)
     drone.move(1.5)
-    drone.land()
-
-    drone.close()
 
 
-# while True:
-# user_input = input("What would you like to do?")
-# if user_input == "Figure 8":
-#     figure_8()
-# elif user_input == "Straight line":
-#     straight_line()
-# elif user_input == "Loop":
-#     loop()
-# elif user_input == "Spiral":
-#     spiral()
-distance_difference = 150
+def hover():
+    drone.hover(3)
 
-drone.pair()
-print(drone.get_bottom_range())
-bottom_range = drone.get_bottom_range()
-print(drone.get_bottom_range())
-drone.takeoff()
-print(drone.get_bottom_range())
-drone.set_throttle(30)
-print(drone.get_bottom_range())
-if distance_difference == int(bottom_range):
-    print(drone.get_bottom_range())
-    drone.set_throttle(-30)
-    print(drone.get_bottom_range())
-drone.land()
-print(drone.get_bottom_range())
-drone.close()
-print(drone.get_bottom_range())
+
+def xcoo():
+    drone = Drone()
+    print(drone.get_pos_y())
+
+
+while True:
+    with mic as source:
+        print("Say something\n")
+        recognizer.adjust_for_ambient_noise(source)  # adjust for noise
+        print("ready to record\n")
+        audio = recognizer.listen(source)  # listens to the person talking
+        print("audio captured\n")
+        try:
+            audio_captured = recognizer.recognize_google(audio)  # sets the outcome to a variable to print
+            print(audio_captured)
+            if audio_captured.upper() == "FIGURE 8":
+                figure_8()
+            elif audio_captured.upper() == "STRAIGHT LINE":
+                straight_line()
+            elif audio_captured.upper() == "LOOP":
+                loop()
+            elif audio_captured.upper() == "SPIRAL":
+                spiral()
+            elif audio_captured.upper() == "HOVER":
+                hover()
+            elif audio_captured.upper() == "xcoo":
+                xcoo()
+            elif audio_captured.upper() == "TAKE OFF":
+                drone.takeoff()
+            elif audio_captured.upper() == "LAND":
+                drone.land()
+            elif audio_captured.upper() == "STOP":
+                drone.close()
+        except sr.UnknownValueError:  # incase we get an error
+            pass
+        except sr.RequestError:
+            pass  # incase we get an error
+
