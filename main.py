86e13535052b0680a8605611e07ab45e4813a32d
MRamirez166 <144700597+MRamirez166@users.noreply.github.com>
2024-01-22 10:13:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# main.py
+# desc: A program for a drone swarm off
+# authors: Martin Ramirez, Naimur Abir, Eric Tavarez, Alejandro Bautista, Connor McGonigal
+# created: 01/09/2024
+
 import speech_recognition as sr
 from codrone_edu.drone import *
 from pydub import AudioSegment
@@ -15,25 +20,32 @@ drone.pair()
 
 
 def straight_line():
-    # Drone goes up for 1 second with 50 power
-    drone.set_pitch(70)
-    drone.avoid_wall(50, 50)
+    # drone goes up for 1 second with 50 power
+    # in order for the drone to take off drone.pair and takeoff must always be set at the beginning of the function
+    drone.pair()
+    drone.takeoff()
+    drone.set_pitch(70)       # the drone will pitch with a power of 70
+    drone.avoid_wall(50, 50)  # this will avoid the wall and time out and stop about 50 cm from a wall
     drone.set_pitch(-70)
-    drone.turn_degree(180)
+    drone.turn_degree(180)    # the drone wil turn at 180 degrees
     drone.set_pitch(70)
-    drone.move(4.5)
+    drone.move(4.5)           # the drone will move for four and a half seconds
+    drone.land()
+    drone.close()
 
 
 
-
+# drone will go in a spiral motion 
 def spiral():
     # default spiral parameters (50, 5, 1)
-    drone.spiral(20, 5, 1)
+    drone.spiral(20, 5, 1)    # the drone will spiral at a speed of 50 for 5 seconds to the right
 
 
 def figure_8():
-    drone.set_yaw(50)
-    drone.set_pitch(70)
+    drone.pair()
+    drone.takeoff()
+    drone.set_yaw(70)   # drone will yaw (turn) at a power of 70
+    drone.set_pitch(50)
     drone.move(1.5)
     drone.set_yaw(-100)
     drone.move(3)
@@ -45,25 +57,22 @@ def figure_8():
     drone.move(3)
     drone.set_yaw(100)
     drone.move(2)
+    drone.land()
+    drone.close()
 
 
 def loop():
     # Drone goes up for 1 second with 50 power
-    drone.set_throttle(50)
+    drone.set_throttle(50)   # the drone will throttle at a power of 50
     drone.set_pitch(50)
-    drone.move(1.5)  # move command executes the movement for 1 second
+    drone.move(1.5)          # move command executes the movement for 1 second
     drone.set_throttle(-50)
     drone.set_pitch(-100)
     drone.move(1.5)
 
-
+# the drone will hover in the air for three seconds 
 def hover():
     drone.hover(3)
-
-
-def xcoo():
-    drone = Drone()
-    print(drone.get_pos_y())
 
 
 while True:
